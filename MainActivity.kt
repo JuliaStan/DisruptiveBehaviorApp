c4b342e6aa4950065772ba8036cf1eb9c5180844
JuliaStan <98061053+JuliaStan@users.noreply.github.com>
2022-02-11 18:44:37 -0800

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.disruptivebehaviorapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+
+        val goToGameMain: Button = findViewById(R.id.goToGameMain)
+        goToGameMain.setOnClickListener {
+            startActivity(Intent(this, GameMainActivity::class.java))
+        }
+    }
+}
